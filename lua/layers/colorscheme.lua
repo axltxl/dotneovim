8c6d5a259dc33d8338eb418db319bda99cd2f021
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2023-09-11 08:45:00 +0200

    gruvbox + colorscheme layer working fine now

--- lua/layers/colorscheme.lua
+++ lua/layers/colorscheme.lua
@@ -2,6 +2,21 @@
 -- layer: color scheme
 --
 
+local core = require('core')
+local keys = require('core.keys')
+
+-- my color themes of choice
+local colorscheme_light = 'github_light'
+local colorscheme_dark  = 'github_dark'
+
+-- toggle between light and dark color themes
+function toggle()
+    if vim.g.colors_name == colorscheme_dark then
+        vim.cmd.colorscheme(colorscheme_light)
+    else
+        vim.cmd.colorscheme(colorscheme_dark)
+    end
+end
 
 -- we need this to export the module
 local m = {}
@@ -9,17 +24,26 @@ local m = {}
 -- list of plugins required by this layer
 function m.get_plugins()
     return {
-        { 'projekt0n/github-nvim-theme' }
+        -- all my color themes go in here ;)
+        { 'projekt0n/github-nvim-theme' },
+        { 'morhetz/gruvbox' }
     }
 end
 
 -- set up configuration for this layer
 function m.setup()
-    -- Set colorscheme
-    local ok, _ = pcall(vim.cmd, 'colorscheme github_dark')
-    if not ok then
-        vim.cmd 'colorscheme default' -- if the above fails, then use default
-    end
+    -- dark by default
+    vim.cmd.colorscheme(colorscheme_dark)
+
+    -- key bindings
+    -- -------------
+    -- toggle between light and dark color themes
+    keys.map_leader_n(';c<space>', function() toggle() end, {desc = "toggle light/dark"})
+
+    -- list color themes on Telescope
+    core.if_mod('telescope', function(telescope)
+        keys.map_leader_n(';cc', function() vim.cmd([[Telescope colorscheme]]) end)
+    end)
 end
 
 -- export the module
